795413d1ebc1c5cd8110e14dda9125b7674889e5
Christopher Lonk <christopher.lonk@phpab.de>
2021-07-15 18:08:33 +0200

    update deploy and env.example

--- product/bin/deploy/local.sh
+++ product/bin/deploy/local.sh
@@ -1,7 +1,9 @@
 cd /vagrant/web/product;
 composer install;
 npm install;
+cp .env.example .env
 php artisan migrate:fresh;
 php artisan db:seed;
 php artisan db:seed product;
 php artisan storage:link;
+php artisan key:generate
